b447553351f1844cc717eb07715e3e7b12380897
timschelpexplore <Tim.Schelpe@xplore.be>
2021-04-29 11:02:55 +0200

    fixed naming

--- src/rest/authorizationRest.js
+++ src/rest/authorizationRest.js
@@ -5,7 +5,7 @@ const RESOURCE_PATH = 'http://localhost:8085/user/'
 
 export default {
     getToken(username, password) {
-        return axios.get(RESOURCE_PATH+"getToken?username="+username+"&password="+password)
+        return axios.get(RESOURCE_PATH+"token?username="+username+"&password="+password)
     },
     register(registration){
         return axios.post(RESOURCE_PATH+"register",registration)
