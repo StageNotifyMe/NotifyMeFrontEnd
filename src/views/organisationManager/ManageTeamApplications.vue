de6eec8e4148f69ebc2fff1c5bbff5340407f086
Arthur De Craemer <arthur.decraemer@student.kdg.be>
2021-06-02 12:24:37 +0200

    respond to team applications

---
+++ src/views/organisationManager/ManageTeamApplications.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page>
+    <h3>Currently using app as: Organisation manager</h3>
+    <h2>Team applications</h2>
+    <TeamApplications />
+  </q-page>
+</template>
+
+<script>
+import TeamApplications from "../../components/organisationManager/UserTeamApplications"
+export default {
+components:{
+    TeamApplications
+},
+}
+</script>
+<style>
+
+</style>
