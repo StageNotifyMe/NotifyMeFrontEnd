3a1cfdb6e32128d1aef16cdba1443b243daf981d
timschelpexplore <Tim.Schelpe@xplore.be>
2021-06-04 10:41:27 +0200

    role checks removed

--- src/views/welcome/WelcomeOmanager.vue
+++ src/views/welcome/WelcomeOmanager.vue
@@ -3,21 +3,7 @@
     <div class="row justify-center">
       <h2>Welcome screen for organisation managers</h2>
     </div>
-    <div class="row justify-center">
-      <p>user has user role: {{hasRole('user')}}</p>
-    </div>
-    <div class="row justify-center">
-      <p>user has admin role: {{hasRole('admin')}}</p>
-    </div>
-    <div class="row justify-center">
-      <p>user has venue_manager role: {{hasRole('venue_manager')}}</p>
-    </div>
-    <div class="row justify-center">
-      <p>user has line_manager role: {{hasRole('line_manager')}}</p>
-    </div>
-    <div class="row justify-center">
-      <p>user has organisation_manager role: {{hasRole('organisation_manager')}}</p>
-    </div>
+    
     <div class="row justify-center q-my-md">
       <q-btn color="secondary" @click="redirectToManage('organisations')" icon-right="groups" label="Manage organisations"></q-btn>
     </div>
--- src/views/welcome/WelcomeUser.vue
+++ src/views/welcome/WelcomeUser.vue
@@ -3,24 +3,6 @@
     <div class="row justify-center">
       <h2>Welcome screen for default users</h2>
     </div>
-    <div class="row justify-center">
-      <p>user has user role: {{ hasRole("user") }}</p>
-    </div>
-    <div class="row justify-center">
-      <p>user has admin role: {{ hasRole("admin") }}</p>
-    </div>
-    <div class="row justify-center">
-      <p>user has venue_manager role: {{ hasRole("venue_manager") }}</p>
-    </div>
-    <div class="row justify-center">
-      <p>user has line_manager role: {{ hasRole("line_manager") }}</p>
-    </div>
-    <div class="row justify-center">
-      <p>
-        user has organisation_manager role:
-        {{ hasRole("organisation_manager") }}
-      </p>
-    </div>
     <div class="row justify-center q-my-md">
       <q-btn
         color="secondary"
