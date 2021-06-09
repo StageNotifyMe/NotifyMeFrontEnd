c8ddc1c641ecae5b0fb4afd5afea164fd84a3e42
timschelpexplore <Tim.Schelpe@xplore.be>
2021-06-09 10:43:40 +0200

    auto refresh for manage lines

--- src/views/venueManager/ManageLines.vue
+++ src/views/venueManager/ManageLines.vue
@@ -1,14 +1,23 @@
 <template>
   <q-page>
-    <BreadCrumbs :locations="['Line Manager','Manage','Lines']"/>
+    <BreadCrumbs :locations="['Line Manager', 'Manage', 'Lines']" />
     <div class="row justify-center">
       <h3>Manage lines</h3>
     </div>
     <div class="row justify-center">
-      <ShowEventLines :eventId="parseInt(eventId)" class="q-ma-md show-table"/>
+      <ShowEventLines
+        :eventId="parseInt(eventId)"
+        class="q-ma-md show-table"
+        :refresh="refreshTable"
+        @refreshed="sendRefresh"
+      />
     </div>
     <div class="row justify-center">
-      <CreateLine :eventId="parseInt(eventId)" class="show-table" />
+      <CreateLine
+        :eventId="parseInt(eventId)"
+        class="show-table"
+        @sendRefresh="sendRefresh"
+      />
     </div>
   </q-page>
 </template>
@@ -16,7 +25,7 @@
 <script>
 import CreateLine from "../../components/venueManager/CreateLine";
 import ShowEventLines from "../../components/venueManager/ShowEventLines";
-import BreadCrumbs from "../../components/BreadCrumbs"
+import BreadCrumbs from "../../components/BreadCrumbs";
 
 export default {
   props: {
@@ -27,7 +36,15 @@ export default {
   },
 
   data() {
-    return {};
+    return {
+      refreshTable: false,
+    };
+  },
+
+  methods: {
+    sendRefresh() {
+      this.refreshTable = !this.refreshTable;
+    },
   },
 
   created() {
